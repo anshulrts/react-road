7407c637fed5a0521e011a4c4570af4f7880ef99
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-26 16:16:53 +0530

    Implemented Search Functionality according to the book

--- src/App.js
+++ src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const list = [
+    {
+        title : 'React',
+        url : 'https://reactjs.org',
+        author : 'Jordan Walke',
+        num_comments : 3,
+        points : 4,
+        objectID : 0
+    },
+    {
+        title : 'Redux',
+        url : 'https://redux.js.org',
+        author : 'Don Abramov, Andrew Clark',
+        num_comments : 2,
+        points : 5,
+        objectID : 1
+    }
+];
+
+const isSearched = searchTerm => item => {
+    return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            list,
+            searchTerm : '',
+        }
+    }
+
+    onSearchChange(event) {
+        this.setState({ searchTerm : event.target.value });
+    }
+
+    render() {
+        const {searchTerm, list} = this.state;
+        return (
+            <div className="App">
+                <form>
+                    <input
+                        type="text"
+                        onChange = { evt => this.onSearchChange(evt) }
+                    / >
+                </form>
+                {
+                    list.filter(isSearched(searchTerm)).map(item =>
+                        {
+                            return (
+                                <div key={item.objectID}>
+                                    <span>
+                                        <a href={item.url}>{item.title}</a>
+                                    </span>
+                                    <span>{item.author}</span>
+                                    <span>{item.num_comments}</span>
+                                    <span>{item.points}</span>
+                                </div>
+                            )
+                        }
+                    )
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
