c487451fded510bca9fc7293ada7c2decb39ce16
Anshul Dahiya <anshul.rts@gmail.com>
2019-02-26 23:14:16 +0530

    Adding styles

--- src/App.js
+++ src/App.js
@@ -49,19 +49,21 @@ class App extends Component {
     render() {
         const { searchTerm, list } = this.state;
         return (
-            <div className="App">
-            <Search
-                value = { searchTerm }
-                onChange = { this.onSearchChange }
-            >
-                {/* Implementing Composable Component using the concept of children */}
-                Search
-            </Search>
-            <Table
-                list = { list }
-                pattern = { searchTerm }
-                onDismiss = { this.onDismiss }
-            />
+            <div className="page">
+                <div className="interactions">
+                    <Search
+                        value = { searchTerm }
+                        onChange = { this.onSearchChange }
+                    >
+                        {/* Implementing Composable Component using the concept of children */}
+                        Search
+                    </Search>
+                </div>
+                <Table
+                    list = { list }
+                    pattern = { searchTerm }
+                    onDismiss = { this.onDismiss }
+                />
             </div>
         );
     }
@@ -77,50 +79,35 @@ const Search = ({ value, onChange, children }) =>
             / >
         </form>
 
-{/* class Search extends Component {
-    render () {
-        //children is used to implement Composable Components
-        const { value, onChange, children } = this.props;
-        return (
-            <form>
-                { children }
-                <input
-                    type="text"
-                    value = { value }
-                    onChange = { onChange }
-                / >
-            </form>
-        );
-    }
-}; */}
-
-class Table extends Component {
-    render () {
-        const { list, pattern, onDismiss } = this.props;
-        return (
-        <div>
+const Table = ({ list, pattern, onDismiss }) =>
+        <div className = "table">
             {
                 list.filter(isSearched(pattern)).map(item =>
-                        <div key={item.objectID}>
-                            <span>
+                        <div key={item.objectID} className="table-row">
+                            <span style={{ width: '40%' }}>
                                 <a href={item.url}>{item.title}</a>
                             </span>
-                            <span>{item.author}</span>
-                            <span>{item.num_comments}</span>
-                            <span>{item.points}</span>
-                            <Button
-                                onClick = { () => onDismiss(item.objectID) }
-                                className = ''
-                            >
-                                Dismiss
-                            </Button>
+                            <span style={{ width: '30%' }}>
+                                {item.author}
+                            </span>
+                            <span style={{ width: '10%' }}>
+                                {item.num_comments}
+                            </span>
+                            <span style={{ width: '10%' }}>
+                                {item.points}
+                            </span>
+                            <span style={{ width: '10%' }}>
+                                <Button
+                                    onClick = { () => onDismiss(item.objectID) }
+                                    className = "button-inline"
+                                >
+                                    Dismiss
+                                </Button>
+                            </span>
                         </div>
                 )
             }
         </div>
-        );
-    }
-};
 
 class Button extends Component {
     render() {
